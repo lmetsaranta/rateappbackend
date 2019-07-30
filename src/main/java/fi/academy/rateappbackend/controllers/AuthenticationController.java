3f6bb19cb7d9e8886356b110025d69c3113dd2af
lmetsaranta <lennu_metsaranta@hotmail.com>
2019-07-30 11:17:07 +0300

    Lisätty AuthenticationController ja Userrepository.

---
+++ src/main/java/fi/academy/rateappbackend/controllers/AuthenticationController.java
@@ -0,0 +1,35 @@
+package fi.academy.rateappbackend.controllers;
+
+import fi.academy.rateappbackend.entities.User;
+import fi.academy.rateappbackend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+@PostMapping("/signup")
+public ResponseEntity<?> createUser(@RequestBody User user) {
+User created = userRepository.save(user);
+    URI location = UriComponentsBuilder.newInstance()
+            .scheme("http")
+            .host("localhost")
+            .port(8080)
+            .path("/users/{id}")
+            .buildAndExpand(created.getId())
+            .toUri();
+    return ResponseEntity.created(location).build();
+}
+
+}
