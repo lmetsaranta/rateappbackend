7cef58b4173b585cf4ab765af7afa76dbfc43f41
lmetsaranta <lennu_metsaranta@hotmail.com>
2019-07-30 10:47:30 +0300

    Ensimmäinen commit - projektin pohja luotu. Application.properties päivitetty.

---
+++ src/main/java/fi/academy/rateappbackend/RateappbackendApplication.java
@@ -0,0 +1,13 @@
+package fi.academy.rateappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateappbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateappbackendApplication.class, args);
+    }
+
+}
