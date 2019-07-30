ee405ddb31e79ae140be0d4a23cc3a4f45f5c210
lmetsaranta <lennu_metsaranta@hotmail.com>
2019-07-30 13:44:20 +0300

    Lisätty Content-entity ja sille controlleri+repository muutamalla toiminnallisuudella

---
+++ src/main/java/fi/academy/rateappbackend/entities/Content.java
@@ -0,0 +1,55 @@
+package fi.academy.rateappbackend.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String headline;
+
+    @NotBlank
+    @Size(max = 255)
+    private String text;
+
+    public Content() {
+    }
+
+    public Content(@NotBlank @Size(max = 40) String headline, @NotBlank @Size(max = 255) String text) {
+        this.headline = headline;
+        this.text = text;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
